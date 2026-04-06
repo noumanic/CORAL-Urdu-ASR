75e0a967f5ec8ee663401012d7e5c64823b8f3dd
Ali Irfan <aliirfanr789@gmail.com>
2026-04-06 16:54:28 +0500

    Voting skip for deletion case removed (logical bug)

--- coral_app/backend/pipeline/coral_pipeline_functions.py
+++ coral_app/backend/pipeline/coral_pipeline_functions.py
@@ -151,7 +151,7 @@ def apply_corrections(align_info, oov_metadata):
             matchinfo = align_info[model]['attempt_matchinfo']
             if (i + voting_skip[model]) >= len(words):
                 continue
-            while (matchinfo[i + voting_skip[model]] == DELETION or matchinfo[i + voting_skip[model]] == INSERTION):
+            while (matchinfo[i + voting_skip[model]] == INSERTION):
                 voting_skip[model] = voting_skip[model] + 1
                 if (i + voting_skip[model]) >= len(words):
                     break
